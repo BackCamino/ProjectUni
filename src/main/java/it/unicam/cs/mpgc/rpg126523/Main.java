cc2d4f5397540780dcf1638e40fd8b6ebcb99ab0
Marco Caminonni <mcaminonni@gmail.com>
2026-07-08 18:11:49 +0200

    add font

--- src/main/java/it/unicam/cs/mpgc/rpg126523/Main.java
+++ src/main/java/it/unicam/cs/mpgc/rpg126523/Main.java
@@ -7,14 +7,30 @@ import it.unicam.cs.mpgc.rpg126523.model.student.Gender;
 import it.unicam.cs.mpgc.rpg126523.model.student.PlayerStudentFactory;
 import it.unicam.cs.mpgc.rpg126523.model.student.Student;
 import it.unicam.cs.mpgc.rpg126523.model.student.StudentFactory;
+import javafx.application.Application;
 
-public class Main {
-    static void main(String[] args) {
-        Student student;
-        StudentFactory studentFactory;
-        studentFactory= new PlayerStudentFactory();
-        StudentClass studentClass = new AthleteFactory();
-        student= studentFactory.createStudent(124535,"marco", Gender.MALE,studentClass);
-        System.out.println(student);
+import javafx.fxml.FXMLLoader;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
     }
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        primaryStage.setTitle("Menu");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/menu.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
 }
