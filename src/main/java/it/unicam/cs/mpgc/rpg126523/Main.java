dcffc238f0a4ea29660c86b13e80f9003be56d38
Marco Caminonni <mcaminonni@gmail.com>
2026-07-08 12:04:37 +0200

    change main fix

--- src/main/java/it/unicam/cs/mpgc/rpg126523/Main.java
+++ src/main/java/it/unicam/cs/mpgc/rpg126523/Main.java
@@ -2,10 +2,11 @@ package it.unicam.cs.mpgc.rpg126523;
 
 import it.unicam.cs.mpgc.rpg126523.model.Gender;
 import it.unicam.cs.mpgc.rpg126523.model.PlayerStudent;
-import it.unicam.cs.mpgc.rpg126523.model.statistics.OverachieverFactory;
-import it.unicam.cs.mpgc.rpg126523.model.statistics.StatisticsFactory;
+
 
 
 public class Main {
-    static void main(String[] args) {}
+    static void main(String[] args) {
+
+    }
 }
