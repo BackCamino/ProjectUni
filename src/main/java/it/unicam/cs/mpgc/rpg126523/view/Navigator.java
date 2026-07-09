28d6c2ed59facca9e4caba769528cfaeb147c70f
Marco Caminonni <mcaminonni@gmail.com>
2026-07-09 16:59:57 +0200

    new file game.fxml

--- src/main/java/it/unicam/cs/mpgc/rpg126523/view/Navigator.java
+++ src/main/java/it/unicam/cs/mpgc/rpg126523/view/Navigator.java
@@ -1,10 +1,12 @@
 package it.unicam.cs.mpgc.rpg126523.view;
 
+import it.unicam.cs.mpgc.rpg126523.model.student.Student;
+
 public interface Navigator {
 
     public void showMenu();
 
     public void showNewPlayerCreation();
 
-    public void showGame();
+    public void showGame(Student player);
 }
