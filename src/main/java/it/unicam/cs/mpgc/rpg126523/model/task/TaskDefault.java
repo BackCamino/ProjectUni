c46a81f2b62ba122130d35780f9316fbd8e3af08
Marco Caminonni <mcaminonni@gmail.com>
2026-07-11 15:05:26 +0200

    implemented

--- src/main/java/it/unicam/cs/mpgc/rpg126523/model/task/TaskDefault.java
+++ src/main/java/it/unicam/cs/mpgc/rpg126523/model/task/TaskDefault.java
@@ -2,19 +2,41 @@ package it.unicam.cs.mpgc.rpg126523.model.task;
 
 public class TaskDefault implements Task {
 
-    private String description;
-    private String name;
+    private final String description;
+    private final String name;
+    private int duration;
 
+    public TaskDefault(String description,String name, int duration){
+        if(description.isEmpty()||name.isEmpty()||duration<0)
+            throw new IllegalArgumentException("Valori non validi per l'oggetto Task");
+        this.name=name;
+        this.description=description;
+        this.duration=duration;
+    }
 
     @Override
-    public TaskResult execute() {
+    public void execute() {
+        generateConsequences();
+    }
 
-        return null;
+    @Override
+    public int getDuration() {
+        return this.duration;
+    }
+
+    @Override
+    public boolean isPossibleToExecute() {
+        return false;
     }
 
     @Override
     public String getDescription() {
-        return "";
+        return description;
 
+    }
+
+    @Override
+    public Consequences generateConsequences() {
+        return null;
     }
 }
